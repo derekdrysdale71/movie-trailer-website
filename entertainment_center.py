4fbc45ffe6cc6f82a9d294ad8f956ed1949987ae
Derek <derek@darthvader.local>
2018-04-05 16:55:24 -0700

    fix variable naming error

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,2 +1,27 @@
 import media
 import fresh_tomatoes
+
+# Movie instances used by fresh_tomatoes
+dead_poets = media.Movie(
+    "Dead Poets Society",
+    "He was their inspiration. He made their lives extraordinary.",
+    "https://images-na.ssl-images-amazon.com/images/I/515v-4X8ZLL.jpg",
+    "https://www.youtube.com/watch?v=wrBk780aOis"
+)
+
+lord_rings = media.Movie(
+    "The Fellowship of the Ring",
+    "One Ring To Rule Them All.",
+    "https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL.jpg",
+    "https://www.youtube.com/watch?v=qyquczkXgPc"
+)
+
+pirates = media.Movie(
+    "The Pirates of the Caribean",
+    "Prepare to be blown out of the water.",
+    "https://images-na.ssl-images-amazon.com/images/I/51fX40gGdXL.jpg",
+    "https://www.youtube.com/watch?v=G7z74BvLWUg"
+)
+
+my_movies = [dead_poets, lord_rings, pirates]
+fresh_tomatoes.open_movies_page(my_movies)
--- media.py
+++ media.py
@@ -6,9 +6,9 @@ class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image = poster_image
-        self.trailer_url = trailer_url
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_url
 
     # Play movie trailer
     def play_trailer(self):
-        webbrowser.open(self.trailer_url)
+        webbrowser.open(self.trailer_youtube_url)
