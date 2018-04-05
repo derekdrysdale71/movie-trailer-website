a92cc358e1cd1ec89b8351e52f09df22aeb918a3
Derek <derek@darthvader.local>
2018-04-05 16:04:32 -0700

    add movie class

--- media.py
+++ media.py
@@ -1,2 +1,12 @@
+import webbrowser
+
+
 class Movie():
-    def __init__(self)
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image = poster_image
+        self.trailer_url = trailer_url
+
+    def play_trailer(self):
+        webbrowser.open(self.trailer_url)
